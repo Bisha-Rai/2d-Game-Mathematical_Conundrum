5178034329f8d7dde8b18eca21f9860373286bd8
Bisha-Rai <bishalchamling14@gmail.com>
2026-01-06 00:28:51 +0545

    add character movement upon user input

--- Gamefile.cc
+++ Gamefile.cc
@@ -4,6 +4,49 @@
 bool running;
 SDL_Window* window=NULL;
 SDL_Renderer* renderer= NULL;
+SDL_Event input;
+
+void userinput(int &x,int &y)
+{
+    
+        SDL_PollEvent(&input);
+        switch(input.type)
+        {
+            case SDL_QUIT:
+                running= false;
+                break;
+     
+            case SDL_KEYDOWN:
+                switch(input.key.keysym.sym)
+                {
+                    case SDLK_a:
+                    x=x-1;
+                
+                    break;
+                    case SDLK_s:
+                    y=y+1;
+                    break;
+                    case SDLK_d:
+                    x=x+1;
+                    break;
+                    case SDLK_w:
+                    y=y-1;
+                    break;
+                    default:
+                    break;
+
+                }
+     
+     
+     
+                default:
+                break;
+     
+     
+     
+            }
+        
+}
 
 class character
 {
@@ -23,6 +66,7 @@ class character
 
 int main(int argc,char* argv[])
 {
+    int x=0,y=0,w=0,h=0;
     character r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,r16,r17,r18,r20,r19,r21,r22,r23,r24,r25,r26,r27,r28,r29,r30,r31,r32,r33,r34,r35,r36,r37,r38,r39,r40,r41,r42,r43,r44,r45;
     if(SDL_Init(SDL_INIT_EVERYTHING)==0)
     {
@@ -39,61 +83,56 @@ int main(int argc,char* argv[])
     {
         SDL_SetRenderDrawColor(renderer,0,0,0,255);
         SDL_RenderClear(renderer);
-        r1.render(500,200,22,10);
-        r2.render(522,210,10,10);
-        r3.render(532,220,10,10);
-        r4.render(532,230,70,10);
-        r5.render(592,220,10,10);
-        r6.render(602,210,10,10);
-        r7.render(612,200,18,10);
-        r8.render(630,210,10,60);
-        r9.render(640,270,10,40);
-        r10.render(630,290,32,10);
-        r11.render(630,310,34,10);
-        r12.render(616,320,14,10);
-        r13.render(630,330,10,10);
-        r14.render(640,340,10,40);
-        r15.render(630,370,10,10);
-        r16.render(620,380,20,10);
-        r17.render(610,340,10,80);
-        r18.render(580,420,45,10);
-        r19.render(580,400,10,20);
-        r20.render(540,400,40,10);
-        r21.render(540,410,10,20);
-        r22.render(510,420,45,10);
-        r23.render(510,390,10,30);
-        r24.render(490,380,40,10);
-        r25.render(460,390,40,10);
-        r26.render(440,380,20,10);
-        r27.render(430,370,10,10);
-        r28.render(420,310,10,60);
-        r29.render(430,300,20,10);
-        r30.render(450,310,10,50);
-        r31.render(490,210,10,80);
-        r32.render(468,290,42,10);
-        r33.render(470,310,40,10);
-        r34.render(490,330,30,10);
-        r35.render(480,340,10,10);
-        r36.render(470,350,10,20);
-        r37.render(480,370,10,10);
-        r38.render(515,360,22,10);
-        r39.render(530,350,10,30);
-        r40.render(602,270,10,30);
-        r41.render(542,270,10,30);
-        r42.render(572,300,20,10);
-        SDL_Event input;
-        SDL_PollEvent(&input);
-        switch(input.type)
-        {
-            case SDL_QUIT:
-                running= false;
-                break;
-            default:
-                break;
-        }
+        userinput(x,y);
+        r1.render(x+500,y+200,22,10);
+        r2.render(x+522,y+210,10,10);
+        r3.render(x+532,y+220,10,10);
+        r4.render(x+532,y+230,70,10);
+        r5.render(x+592,y+220,10,10);
+        r6.render(x+602,y+210,10,10);
+        r7.render(x+612,y+200,18,10);
+        r8.render(x+630,y+210,10,60);
+        r9.render(x+640,y+270,10,40);
+        r10.render(x+630,y+290,32,10);
+        r11.render(x+630,y+310,34,10);
+        r12.render(x+616,y+320,14,10);
+        r13.render(x+630,y+330,10,10);
+        r14.render(x+640,y+340,10,40);
+        r15.render(x+630,y+370,10,10);
+        r16.render(x+620,y+380,20,10);
+        r17.render(x+610,y+340,10,80);
+        r18.render(x+580,y+420,45,10);
+        r19.render(x+580,y+400,10,20);
+        r20.render(x+540,y+400,40,10);
+        r21.render(x+540,y+410,10,20);
+        r22.render(x+510,y+420,45,10);
+        r23.render(x+510,y+390,10,30);
+        r24.render(x+490,y+380,40,10);
+        r25.render(x+460,y+390,40,10);
+        r26.render(x+440,y+380,20,10);
+        r27.render(x+430,y+370,10,10);
+        r28.render(x+420,y+310,10,60);
+        r29.render(x+430,y+300,20,10);
+        r30.render(x+450,y+310,10,50);
+        r31.render(x+490,y+210,10,80);
+        r32.render(x+468,y+290,42,10);
+        r33.render(x+470,y+310,40,10);
+        r34.render(x+490,y+330,30,10);
+        r35.render(x+480,y+340,10,10);
+        r36.render(x+470,y+350,10,20);
+        r37.render(x+480,y+370,10,10);
+        r38.render(x+515,y+360,22,10);
+        r39.render(x+530,y+350,10,30);
+        r40.render(x+602,y+270,10,30);
+        r41.render(x+542,y+270,10,30);
+        r42.render(x+572,y+300,20,10);
         SDL_RenderPresent(renderer);
+        
 
     }
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
     
 
     return 0;
