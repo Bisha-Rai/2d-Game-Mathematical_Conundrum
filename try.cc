0543c142f47bf61e66a518bbebfe95cded297dfd
Bisha-Rai <bishalchamling14@gmail.com>
2026-04-18 13:11:43 +0545

    Update 1

---
+++ try.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,i;
+    for(i=0;i++;i<10)
+    {
+        printf("enter value of a: \n");
+        scanf("%d",&a);
+        printf("&d \n",a);
+
+    }
+}
